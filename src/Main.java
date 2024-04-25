667ca9faf7e6b78e0b4a8668981b3905212bef79
yulia <dementevau21@email.com>
2024-04-25 18:26:37 +0300

    QA79

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
-        {
-            int ticketprice = 5000; //стоимость билета
 
-            int rubles = 20; //кол-во рублей для одной бонусной мили
 
-            int bonus = ticketprice / rubles;
-            System.out.println("Итоговый бонус:" + bonus);
+        int ticketPrice = 5000; //стоимость билета
 
-        }
+        int rubles = 20; //кол-во рублей для одной бонусной мили
+
+        int bonus = ticketPrice / rubles;
+        System.out.println("Итоговый бонус:" + bonus);
+
+
     }
 }
